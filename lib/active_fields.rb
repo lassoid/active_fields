33281e3ec54c13bd1590629ee41bbb382fb90fa6
Kirill Usanov <kirill.usanov.dev@gmail.com>
2024-09-11 01:13:35 +0300

    Eager load all

--- lib/active_fields.rb
+++ lib/active_fields.rb
@@ -6,46 +6,60 @@ require_relative "active_fields/engine"
 module ActiveFields
   extend ActiveSupport::Autoload
 
-  autoload :Config
-  autoload :CustomizableConfig
-  autoload :HasActiveFields
+  class << self
+    def eager_load!
+      super
+      Casters.eager_load!
+      Validators.eager_load!
+    end
+  end
+
+  eager_autoload do
+    autoload :Config
+    autoload :CustomizableConfig
+    autoload :HasActiveFields
+  end
 
   module Casters
     extend ActiveSupport::Autoload
 
-    autoload :BaseCaster
-    autoload :BooleanCaster
-    autoload :DateCaster
-    autoload :DateArrayCaster
-    autoload :DateTimeCaster
-    autoload :DateTimeArrayCaster
-    autoload :DecimalCaster
-    autoload :DecimalArrayCaster
-    autoload :EnumCaster
-    autoload :EnumArrayCaster
-    autoload :IntegerCaster
-    autoload :IntegerArrayCaster
-    autoload :TextCaster
-    autoload :TextArrayCaster
+    eager_autoload do
+      autoload :BaseCaster
+      autoload :BooleanCaster
+      autoload :DateCaster
+      autoload :DateArrayCaster
+      autoload :DateTimeCaster
+      autoload :DateTimeArrayCaster
+      autoload :DecimalCaster
+      autoload :DecimalArrayCaster
+      autoload :EnumCaster
+      autoload :EnumArrayCaster
+      autoload :IntegerCaster
+      autoload :IntegerArrayCaster
+      autoload :TextCaster
+      autoload :TextArrayCaster
+    end
   end
 
   module Validators
     extend ActiveSupport::Autoload
 
-    autoload :BaseValidator
-    autoload :BooleanValidator
-    autoload :DateValidator
-    autoload :DateArrayValidator
-    autoload :DateTimeValidator
-    autoload :DateTimeArrayValidator
-    autoload :DecimalValidator
-    autoload :DecimalArrayValidator
-    autoload :EnumValidator
-    autoload :EnumArrayValidator
-    autoload :IntegerValidator
-    autoload :IntegerArrayValidator
-    autoload :TextValidator
-    autoload :TextArrayValidator
+    eager_autoload do
+      autoload :BaseValidator
+      autoload :BooleanValidator
+      autoload :DateValidator
+      autoload :DateArrayValidator
+      autoload :DateTimeValidator
+      autoload :DateTimeArrayValidator
+      autoload :DecimalValidator
+      autoload :DecimalArrayValidator
+      autoload :EnumValidator
+      autoload :EnumArrayValidator
+      autoload :IntegerValidator
+      autoload :IntegerArrayValidator
+      autoload :TextValidator
+      autoload :TextArrayValidator
+    end
   end
 
   class << self
