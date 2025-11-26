bee0097e620332ed42727801901d6bc04e283d78
Kirill Usanov <kirill.usanov.dev@gmail.com>
2025-11-26 15:14:54 +0300

    Fix users active fields finders form url with tenant_id

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :users, only: %i[index new create edit update destroy]
+  get "tenants/:tenant_id/users", to: "users#index", as: :tenant_users
   resources :authors, only: %i[index new create edit update destroy]
   resources :groups, only: %i[index new create edit update destroy]
   resources :posts, only: %i[index new create edit update destroy]
