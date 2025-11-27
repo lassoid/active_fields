cce777604559db8b3a2d09d561eac9b12acc6c48
Kirill Usanov <kirill.usanov.dev@gmail.com>
2025-11-27 12:25:40 +0300

    Debug

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -2,6 +2,6 @@
 
 # Set up gems listed in the Gemfile.
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
-
+p ENV["BUNDLE_GEMFILE"], File.exist?(ENV["BUNDLE_GEMFILE"])
 require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
 $LOAD_PATH.unshift File.expand_path("../../../lib", __dir__)
