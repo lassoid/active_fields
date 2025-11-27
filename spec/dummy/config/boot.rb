df381c2ac05198551458d40ae6f238f4c39f059b
Kirill Usanov <kirill.usanov.dev@gmail.com>
2025-11-27 12:55:18 +0300

    Improve Dummy boot script to properly handle relative path in BUNDLE_GEMFILE

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+# Convert relative path to absolute, using project root as base
+ENV["BUNDLE_GEMFILE"] = File.expand_path(
+  ENV["BUNDLE_GEMFILE"] || "Gemfile",
+  File.expand_path("../../..", __dir__),
+)
+
 # Set up gems listed in the Gemfile.
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
-p ENV["BUNDLE_GEMFILE"], File.exist?(ENV["BUNDLE_GEMFILE"])
 require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+
 $LOAD_PATH.unshift File.expand_path("../../../lib", __dir__)
