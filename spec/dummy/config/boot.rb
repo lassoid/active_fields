8121a7f9f53d6af99d31c65ec7f0f17800f234cb
Kirill Usanov <kirill.usanov.dev@gmail.com>
2025-11-27 13:48:53 +0300

    Try fix

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,18 +1,7 @@
 # frozen_string_literal: true
 
-p ENV["BUNDLE_GEMFILE"]
-p __dir__
-p File.expand_path("../../..", __dir__)
-
-# Convert relative path to absolute, using project root as base
-ENV["BUNDLE_GEMFILE"] = File.expand_path(
-  ENV["BUNDLE_GEMFILE"] || "Gemfile",
-  File.expand_path("../../..", __dir__),
-)
-
-p ENV["BUNDLE_GEMFILE"]
-
 # Set up gems listed in the Gemfile.
-require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
 
+require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
 $LOAD_PATH.unshift File.expand_path("../../../lib", __dir__)
